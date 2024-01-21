5b6619ac837bff9db942d7b31b2fa35840623aac
ines <85657519+ines901231@users.noreply.github.com>
2024-01-21 09:33:32 +0800

    navbar logout

--- gallery/index.php
+++ gallery/index.php
@@ -29,6 +29,7 @@ if(!isset($_SESSION["login"])) {
             <li><a href="../gallery/index.php">Gallery</a></li>
             <li><a href="../payment/index.php">Pricing</a></li>
             <li><a href="../contact/index.php">Feedback</a></li>
+            <li><a class="logot" href="../admin/logout.php">Log Out</a></li>
         </ul>
     </nav>
 
